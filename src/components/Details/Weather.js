1194057a51858bc88afe38d38067e4d08357d2e9
0tuedon <tuedonmofe@gmail.com>
2021-11-14 01:18:51 +0100

    Fixed OverFlow for Mobile

--- src/components/Details/Weather.js
+++ src/components/Details/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import style from './Weather.module.css';
-
+import raining from '../../assets/images/Rainny.png'
 
 const Weather = ()=> 
 {
@@ -9,10 +9,10 @@ const Weather = ()=>
         <React.Fragment>
         <div className={style.container}>
             <div className={style['container-item']}>
-                Icon
+                <img className={style['container-item-image']} src={raining} alt="raining"/>
             </div>
             <div className={style['container-item']}>
-                <div>21<sup>o</sup></div>
+                <div className={style['container-item-temp']}>21<sup>o</sup></div>
                 <div>Mostly Sunny</div>
             </div>
         </div>
